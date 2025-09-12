9a644903cc48496eded0aded41ae5a953cd754dc
Blactot <patrick.louvel@gmail.com>
2025-09-12 11:48:34 +0200

    Update Google Maps embed URL on contact page
    Replaced the Google Maps iframe src with a simplified embed URL for Denneville-Plage. This change improves maintainability and readability of the embedded map configuration.

--- src/app/contact/page.tsx
+++ src/app/contact/page.tsx
@@ -37,9 +37,9 @@ export default async function ContactPage({ searchParams }: { searchParams: { su
                 </Card>
                  <Card className="aspect-[16/9]">
                     <CardContent className="p-0 h-full">
-                         <iframe 
-                            src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2607.243423374898!2d-1.7719688846278856!3d49.20091877932236!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x480c5f7d1e1f2a3b%3A0x1b7b7b1b3b1b7b7b!2sDenneville-Plage%2C%2050580%20Denneville!5e0!3m2!1sfr!2sfr!4v1622540000000!5m2!1sfr!2sfr" 
-                            width="100%" 
+                         <iframe
+                            src="https://maps.google.com/maps?q=Denneville-Plage,50580&t=&z=15&ie=UTF8&iwloc=&output=embed"
+                            width="100%"
                             height="100%"
                             style={{ border: 0 }}
                             allowFullScreen={false}
